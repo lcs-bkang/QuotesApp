c7c17294a7707bc00b1aaa02dcafc26073ffb14a
Bradley Kang <bkang@lcs.on.ca>
2021-10-06 10:55:49 -0400

    Initial Commit

---
+++ QuotesApp/QuotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesAppApp.swift
+//  QuotesApp
+//
+//  Created by Brad Kang on 2021-10-06.
+//
+
+import SwiftUI
+
+@main
+struct QuotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
